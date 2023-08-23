127796fea3a5ad5458bbb4d572fa566cb2bf10cb
Aditya Shende <aditya.shende@lief.care>
2023-08-24 05:08:34 +0530

    creating a basic query to find contacts using prisma

--- src/routes/identify.ts
+++ src/routes/identify.ts
@@ -1,15 +1,56 @@
 import express, { Express, Request, Response, Router } from "express";
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma: PrismaClient = new PrismaClient();
 
 const router: Router = Router();
 
-router.post("/", async (req: Request, res: Response) => {
-  const { email, phoneNumber } = req.body;
+interface requestFields {
+  email?: string;
+  phoneNumber?: number;
+}
 
-  const contact = await prisma.contact.create({ data: req.body });
-  res.json(contact);
+interface responseFields {
+  primaryContactId?: number;
+  emails?: string[];
+  phoneNumbers?: string[];
+  secondaryContactIds?: number[];
+}
+
+router.post("/", async (req: Request, res: Response) => {
+  const { email, phoneNumber }: requestFields = req.body;
+
+  const contactSelect: Prisma.ContactSelect = {
+    id: true,
+    email: true,
+    phoneNumber: true,
+    linkedId: true,
+    linkPrecedence: true,
+    deletedAt: true,
+    createdAt: true,
+    updatedAt: true,
+  };
+
+  const contacts = await prisma.contact.findMany({
+    where: {
+      OR: [
+        {
+          email: {
+            equals: email,
+          },
+        },
+        {
+          phoneNumber: {
+            equals: phoneNumber?.toString(),
+          },
+        },
+      ],
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+    select: contactSelect,
+  });
 });
 
 module.exports = router;
